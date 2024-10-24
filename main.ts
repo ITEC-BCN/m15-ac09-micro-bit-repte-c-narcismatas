46484ab62c5856dd8a0e5bfb24d8dd996b90cd56
Narcis Matas <146827232+narcismatas@users.noreply.github.com>
2024-10-24 17:41:59 +0200

    Extra

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
-basic.forever(function () {
-	
+let Resultado = 0
+radio.onReceivedNumber(function (receivedNumber) {
+    Resultado = randint(1, 6)
+    radio.sendNumber(Resultado)
+    if (receivedNumber > Resultado) {
+        basic.showIcon(IconNames.Sad)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("Wasaaaa")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    Resultado = randint(1, 6)
+    radio.sendNumber(Resultado)
 })
